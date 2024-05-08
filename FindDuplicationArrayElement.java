3c65ab7371a045e02b88fe69133d9f5a4b44b093
K DHYANA SAMAGA <156940188+Dhyana2004@users.noreply.github.com>
2024-05-08 11:19:56 +0530

    Update and rename FindDuplicationArray.java to FindDuplicationArrayElement.java

--- FindDuplicationArrayElement.java
+++ FindDuplicationArrayElement.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FindDuplicationArray {
+public class FindDuplicationArrayElement {
     /**
      * Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array.
      Return the answer in ascending order. If no such element is found, return list containing [-1].
